638333fd220ca74be18da53de266d3a89bff1522
Gage <gagemigan@gmail.com>
2020-06-30 11:10:08 -0400

    anotherone

--- app.js
+++ app.js
@@ -1,71 +1,61 @@
 
 const apikey = 'PUBdaeTi76jawwrtS0wJrCNQhEb59H9slyuoHqOQ';
-const searchURL = 'https://developer.nps.gov/api/v1/parks';
-//date->fullname->discretion
+//const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
  
  
+function template(call){
+  const arr =[];
+  console.log('works');
+  call.data.map(park =>{
+    arr.push(`
+            <li>
+                <h2>${park.fullName}</h2>
+                <a href="${park.url}">${park.url}</a>
+                <p>${park.description}</p>
+            </li>`);
+  }
+  );
+  $('#results-list').html(arr.join(''));
+} 
  
+function parkReserver(q,l){
  
-function parkReserver(){
- 
- 
-  fetch(`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${apikey}`)
+  fetch(`https://developer.nps.gov/api/v1/parks?q=${q}&limit=${l}&api_key=${apikey}`)
     .then(response =>{
       if (response.ok){
+        console.log('works');
         return response.json();
       }
       throw new console.error('woops');
         
     })
     .then(jsonData => {
-      extractData(jsonData);
+      template(jsonData);
     })
     .catch(e => console.log(e));
 } 
- 
-/** 
-* @param {string} fullname
-* @param {URL} html_url
-* @param {string} description
-* @param {date} the JSON data
-*/
- 
-const getParks = function(data){
-  data.forEach(park => {
-    let{
-      fullname,
-      url,
-      description
-    } = park;
-    
-    
 
-            
-    $('results-list').append(template(fullname,url,description));
-  }
-  );
-    
-};
- 
-function template(fullname, url, description){
-  let t = `
-            <li>
-                <h2>${fullname}</h2>
-                <a href="${url}">${url}</a>
-                <p>${description}</p>
-            </li>`;
-  return t; 
+const onClick =()=> {
+  $('.national-park-search').submit( (e) => {
+    e.preventDefault();
+    console.log('works');
+    const q = $('#national-park-name').val();
+    const l = $('#d-count').val();
+    parkReserver(q,l);
+  })
 }
  
- 
- 
- 
 function watchForm() {
-  $('form').submit(e =>{
-    e.preventDefault;
-
-        
-  });
+  onClick();
+  // $('#go').submit(e =>{
+  //   e.preventDefault;
+  //   console.log('works');
+  //   const q = $('#national-park-name').val();
+  //   const l = $('#d-count').val();
+  //   parkReserver(q,l);
+    
+  // });
 }
  
  
